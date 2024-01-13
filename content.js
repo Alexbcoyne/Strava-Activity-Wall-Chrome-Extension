a42c9b1499ddd4501b9d12daf6297808f74f154f
Alexbcoyne <alexbcoyne@gmail.com>
2024-01-12 22:31:29 -0500

    reduce icon size

--- content.js
+++ content.js
@@ -4,14 +4,14 @@ const li = document.createElement("li");
 const img = document.createElement("img");
 
 if (ul) {
-  li.className = "nav-item";
+  li.className = "nav-item drop-down-menu";
   li.id = "activity-wall";
 
   img.src = chrome.runtime.getURL("images/icon1.png");
   img.alt = "Activity Wall";
 
-  img.style.width = "32px";
-  img.style.height = "32px";
+  img.style.width = "24px";
+  img.style.height = "24px";
 
   li.appendChild(img);
   ul.appendChild(li);
